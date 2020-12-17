51dc0019e12fea45ac714913cf437adfc3aee092
Quiana <quianalee@Quianas-MacBook-Air.local>
2020-12-17 15:43:37 -0500

    First Submit

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const morgan = require('morgan')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const data = require('./data.json');
 
-const app = express()
 
-app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('You will come out stronger than you went in!')
-})
 
-const PORT = 8000
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+  })
+  
+  app.get('/movies', function handleGetMovie(req, res) {
+    let response = data;
+  
+    if (req.query.genre) {
+      response = response.filter(movie =>
+        movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+      )
+    }
+  
+    if (req.query.country) {
+      response = response.filter(movie =>
+        movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+      )
+    }
+  
+    if (req.query.avg_vote) {
+      response = response.filter(movie =>
+        Number(movie.avg_vote) >= Number(req.query.avg_vote)
+      )
+    }
+  
+    res.json(response)
+  })
+  
+  const PORT = 8000
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+  })
+  
